dc35234c2a9d5e77a72343234ba1a11b0608ec25
G-Subramani <subramanig064@gmail.com>
2024-12-08 16:07:15 +0530

    folder structure created

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Drug-Discovery-using-GANs-
+
+'''bash 
+Project repo:https://github.com/
+## step 1-Ctreate conda environment after opening the repository 
+'''bash 
+conda create -n drds python=3.8 -y
+
+'''bash 
+conda activate drds
+'''
+
+install the requirements 
+'''bash 
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Drug-Discovery-GANs",
+    version="0.0.1",
+    author="Subramani G",
+    author_email="subramanig064@gmail.com",
+    description="Drug discovery project using GANs.",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
